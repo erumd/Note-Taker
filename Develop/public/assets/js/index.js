322d1ee39ef90071f7e79c9e3b63ab9426be9dac
Erum Dhukka <erumdhukka531@gmail.com>
2021-02-11 20:35:25 -0600

    index.js

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -96,7 +96,6 @@ const handleNoteDelete = (e) => {
 };
 
 // Sets the activeNote and displays it
-
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
