a4a16eaaffe2b397e07f903c4be53251f2ca271e
Erum Dhukka <erumdhukka531@gmail.com>
2021-02-11 20:26:37 -0600

    index.js

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -117,6 +117,7 @@ const handleRenderSaveBtn = () => {
 };
 
 // Render the list of note titles
+
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
