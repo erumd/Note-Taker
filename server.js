9b44c3194a99ab18fd87915649e3d20a2ea887fd
Erum Dhukka <erumdhukka531@gmail.com>
2021-02-21 19:51:46 -0600

    write pseudo code

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-
 //finds folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-//express app that takes data 
+//express app that takes data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //gets saved notes and put in db.json
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"))
+  res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-//add notes to db.jason 
+//add notes to db.jason
 
+//deleting notes
+
+//calls the index.html
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+//call notes.html
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
