26c019b42ce4a7520cd7f3086fb8fedf659c336c
Erum Dhukka <erumdhukka531@gmail.com>
2021-02-23 14:46:30 -0600

    updated after local sever working

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-
 const express = require("express");
 const path = require("path");
 
 // Sets up the Express App
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 //finds folder
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 //express app that takes data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //gets saved notes and put in db.json
-app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/db/db.json"));
-});
+// app.get("/api/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/db/db.json"));
+// });
 
 //add notes to db.jason
 
@@ -25,21 +24,21 @@ app.get("/api/notes", (req, res) => {
 
 //calls the index.html. Star Wars Heruko activity
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "develop/public/index.html"))
+);
 
 //call notes.html
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "develop/public/notes.html"))
+);
 
-//used index.js to display all notes 
-app.get("/public/assets/js/index.js", (req, res) => res.json(getAndRenderNotes));
+//used index.js to display all notes
+app.get("/public/assets/js/index.js", (req, res) =>
+  res.json(getAndRenderNotes)
+);
 
 // listen request
 // Starts our server.
 //changed server to app.listen
-app.listen(PORT, () => {
-  console.log(`Server is listening on PORT: ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
